8a44f188e7321dac92f895dbf8d53effc55623f6
jtkaufman737 <jtkaufman737@gmail.com>
2016-09-11 13:51:51 -0400

    Updated switch

--- Desktop/side_projects/php/phpSwitch.php
+++ Desktop/side_projects/php/phpSwitch.php
@@ -18,3 +18,24 @@
     ?>
     </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+    $fruit = "Apple";
+
+    switch ($fruit) {
+        case'Apple':
+            echo "Yummy.";
+            break;
+        default:
+            echo "Not an apple";
+    }
+
+    ?>
+    </body>
+</html>
