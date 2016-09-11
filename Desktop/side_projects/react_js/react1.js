6f2d63f3cf25b6a33b9f1d7a626b5946e79ae038
jtkaufman737 <jtkaufman737@gmail.com>
2016-09-11 18:16:48 -0400

    Require, render, and other basics

--- Desktop/side_projects/react_js/react1.js
+++ Desktop/side_projects/react_js/react1.js
@@ -7,3 +7,25 @@ var myDiv= (
     <h1>Hello world</h1>;
   </div>
 );
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="/styles.css">
+	<title>Learn ReactJS</title>
+</head>
+
+<body>
+  <span id="container"></span>
+	<script src="https://s3.amazonaws.com/codecademy-content/courses/React/react-course-bundle.min.js"></script>
+  <script src="/app.compiled.js"></script>
+</body>
+
+</html>
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Write code here:
+ReactDOM.render(<h1>Render me!</h1>, document.getElementById('app'));
