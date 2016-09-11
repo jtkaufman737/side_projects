328c842d1106e68ec832d8a9266da0e693b21dd7
jtkaufman737 <jtkaufman737@gmail.com>
2016-09-11 17:11:08 -0400

    Count and pushing to arrays

---
+++ Desktop/side_projects/php/arrayPushAndCount.php
@@ -0,0 +1,14 @@
+<html>
+    <p>
+	<?php
+	$myArray=array(1,2,3,4,5);
+	array_push($myArray,10);
+	array_push($myArray,20);
+	array_push($myArray,30);
+	array_push($myArray,40);
+	array_push($myArray,50);
+
+	print count($myArray);
+	?>
+	</p>
+</html>
