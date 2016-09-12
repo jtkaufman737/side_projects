6e9452493646b360cd58831658bdd96432ef8c69
jtkaufman737 <jtkaufman737@gmail.com>
2016-09-12 07:42:46 -0400

    Self close tags

---
+++ Desktop/side_projects/react_js/react4.js
@@ -0,0 +1,16 @@
+var profile = (
+  <div>
+    <h1>I AM JENKINS</h1>
+    <img src="images/jenkins.png"/>
+    <article>
+      I LIKE TO SIT
+      <br/>
+      JENKINS IS MY NAME
+      <br/>
+      THANKS HA LOT
+    </article>
+  </div>
+);
+
+// Example of how you have to close out self closing tags
+// with / ex <br/> and can't simply do <br>
