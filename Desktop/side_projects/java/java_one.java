d3907ea777ff7ee002509897a786853ac32c3e39
jtkaufman737 <jtkaufman737@gmail.com>
2016-09-15 13:08:55 -0400

    Java one

---
+++ Desktop/side_projects/java/java_one.java
@@ -0,0 +1,23 @@
+public class Comments {
+	public static void main(String[] args) {
+
+//		System.out.println("Noise!");
+
+/*COMMENTY
+COMMENTY
+COMMENT*/
+
+
+
+
+	}
+}
+
+public class Modulo {
+	public static void main(String[] args) {
+
+		int myRemainder = 10 % 4;
+		System.out.println(myRemainder);
+
+	}
+} 
